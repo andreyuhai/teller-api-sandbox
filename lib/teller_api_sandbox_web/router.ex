49afad62c5b7a42f56f3e4e5b6705316f313b105
Burak <burak@llinformatics.com>
2021-08-05 22:33:55 +0200

    Implement assign_state_from_api_token/2, and decode_api_token_from_basic_auth/1

--- lib/teller_api_sandbox_web/router.ex
+++ lib/teller_api_sandbox_web/router.ex
@@ -21,4 +21,21 @@ defmodule TellerApiSandboxWeb.Router do
     password = System.fetch_env!("TELLER_PASSWORD")
     Plug.BasicAuth.basic_auth(conn, username: username, password: password)
   end
+
+  defp assign_state_from_api_token(conn, _opts) do
+    decoded_state =
+      conn
+      |> get_req_header("authorization")
+      |> decode_api_token_from_basic_auth()
+
+    conn
+    |> assign(:state, decoded_state)
+  end
+
+  defp decode_api_token_from_basic_auth(["Basic " <> base64_credentials]) do
+    {:ok, "test_" <> credentials} = Base.decode64(base64_credentials)
+    [api_token, pass]= String.split(credentials, ":")
+    {:ok, %{} = verified} = Plug.Crypto.verify("foo", "bar", api_token)
+    verified
+  end
 end
