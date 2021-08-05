d99658c6b054b1f98005f5e28d6b29e1c020cea4
Burak <burak@llinformatics.com>
2021-08-05 22:35:08 +0200

    Comment out the socket and plug Plug.Static

--- lib/teller_api_sandbox_web/endpoint.ex
+++ lib/teller_api_sandbox_web/endpoint.ex
@@ -10,19 +10,19 @@ defmodule TellerApiSandboxWeb.Endpoint do
     signing_salt: "ZEYFSyPp"
   ]
 
-  socket "/socket", TellerApiSandboxWeb.UserSocket,
-    websocket: true,
-    longpoll: false
+  # socket "/socket", TellerApiSandboxWeb.UserSocket,
+  #   websocket: true,
+  #   longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :teller_api_sandbox,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+  # plug Plug.Static,
+  #   at: "/",
+  #   from: :teller_api_sandbox,
+  #   gzip: false,
+  #   only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
