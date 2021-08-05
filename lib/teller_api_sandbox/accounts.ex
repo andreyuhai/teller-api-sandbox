6fc7b17f811baa221e022be0ed065064888316fb
Burak <burak@llinformatics.com>
2021-08-06 00:41:13 +0200

    Implement generate_account_data/1, not yet complete

--- lib/teller_api_sandbox/accounts.ex
+++ lib/teller_api_sandbox/accounts.ex
@@ -1,3 +1,29 @@
 defmodule TellerApiSandbox.Accounts do
+  def generate_account_data(state) do
+    :rand.seed(:exsplus, state.random_seed)
 
+    %{
+      account_number: 112233445566,
+      balances: %{
+        available: 1256.31,
+        ledger: 1256.31
+      },
+      currency_code: "USD",
+      enrollment_id: "test_enr_1xyG_97e",
+      id: state.account_id,
+      institution: %{
+        id: "teller_bank",
+        name: "The Teller Bank"
+      },
+      links: %{
+        "self": "http://localhost/accounts/test_acc_E6kuc45U",
+        transactions: "http://localhost/accounts/test_acc_E6kuc45U/transactions"
+      },
+      name: "Test Checking Account",
+      routing_numbers: %{
+        ach: "864952590",
+        wire: "124952590"
+      }
+    }
+  end
 end
