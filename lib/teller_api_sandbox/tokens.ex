4458f04e2a83019ab01c90b94f7493788d76e372
Burak <burak@llinformatics.com>
2021-08-07 18:55:27 +0200

    Rename generate/1 -> generate_api_token/1, rename decode_state_from/1 -> decode_api_token/1

--- lib/teller_api_sandbox/tokens.ex
+++ lib/teller_api_sandbox/tokens.ex
@@ -1,10 +1,10 @@
 defmodule TellerApiSandbox.Tokens do
 
-  def generate(%{} = state) do
+  def generate_api_token(%{} = state) do
     "test_" <> Plug.Crypto.sign("foo", "bar", state)
   end
 
-  def decode_state_from("test_" <> credentials) do
+  def decode_api_token("test_" <> credentials) do
     [api_token, pass] = String.split(credentials, ":")
     {:ok, %{} = verified_state} = Plug.Crypto.verify("foo", "bar", api_token)
     verified_state
