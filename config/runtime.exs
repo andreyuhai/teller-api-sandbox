08f963f9711740ee1217ca47714b764590671578
Burak <burak@llinformatics.com>
2021-08-07 18:52:29 +0200

    Only encode account_seed into API token

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 alias TellerApiSandbox.Tokens
 
-token = Tokens.generate(%{account_id: "test_acc_E6kuc45U", random_seed: :rand.uniform(200)})
+token = Tokens.generate_api_token(%{account_seed: :rand.uniform(200)})
 
 System.put_env("TELLER_USERNAME", token)
 System.put_env("TELLER_PASSWORD", "")
