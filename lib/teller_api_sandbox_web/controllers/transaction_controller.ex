b15ae37493a1b4fd4ce15a4dbed92c0e4fb419ba
Burak <burak@llinformatics.com>
2021-08-07 23:31:41 +0200

    Implement show/2 action for /accounts/:account_id/transactions/:transaction_id

--- lib/teller_api_sandbox_web/controllers/transaction_controller.ex
+++ lib/teller_api_sandbox_web/controllers/transaction_controller.ex
@@ -5,15 +5,28 @@ defmodule TellerApiSandboxWeb.TransactionController do
 
   def index(conn, params) do
     state = conn.assigns.state
+    account_id = params["account_id"]
 
-    if Accounts.valid_account_id?(state, params["account_id"]) do
-      json(conn, Transactions.generate_transactions(state))
+    if Accounts.valid_account_id?(state, account_id) do
+      json(conn, Transactions.generate_transactions(state, account_id))
     else
       json(conn, %{error: "Invalid account id"})
     end
   end
 
-  def show(conn, _params) do
-    json(conn, Transactions.generate_transaction(conn.assigns.state))
+  def show(conn, params) do
+    state = conn.assigns.state
+    account_id = params["account_id"]
+    transaction_id = params["transaction_id"]
+
+    if Transactions.valid_transaction_id?(transaction_id, account_id,state.seed) do
+      {:ok, d} = Base.decode64(transaction_id)
+      <<days_ago::binary-size(2), seed::binary>> = d
+      {days_ago, ""} = Integer.parse(days_ago)
+
+      json(conn, Transactions.generate_transaction(state.seed, account_id, days_ago))
+    else
+      json(conn, %{error: "Invalid transaction id"})
+    end
   end
 end
