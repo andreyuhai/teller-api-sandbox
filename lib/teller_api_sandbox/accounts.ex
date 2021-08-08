8d8730d83c62f433483ac2c102246152241296ec
Burak <burak@llinformatics.com>
2021-08-08 02:28:57 +0200

    Change keys from atom to string to be able to test easier

--- lib/teller_api_sandbox/accounts.ex
+++ lib/teller_api_sandbox/accounts.ex
@@ -7,7 +7,7 @@ defmodule TellerApiSandbox.Accounts do
   def valid_account_id?(state, account_id) do
     account_data = generate_account_data(state)
 
-    account_data.id == account_id
+    account_data["id"] == account_id
   end
 
   def generate_account_data(state) do
@@ -16,18 +16,18 @@ defmodule TellerApiSandbox.Accounts do
     account_id = account_id()
 
     %{
-      balances: balances,
-      account_number: account_number(),
-      currency_code: currency_code(),
-      enrollment_id: enrollment_id(),
-      id: account_id,
-      institution: institution(),
-      links: %{
-        "self": "http://localhost/accounts/#{account_id}",
-        transactions: "http://localhost/accounts/#{account_id}/transactions"
+      "balances" => balances,
+      "account_number" => account_number(),
+      "currency_code" => currency_code(),
+      "enrollment_id" => enrollment_id(),
+      "id" => account_id,
+      "institution" => institution(),
+      "links" => %{
+        "self" => "http://localhost/accounts/#{account_id}",
+        "transactions" => "http://localhost/accounts/#{account_id}/transactions"
       },
-      name: "Test Checking Account",
-      routing_numbers: routing_numbers()
+      "name" => "Test Checking Account",
+      "routing_numbers" => routing_numbers()
     }
   end
 
@@ -39,8 +39,8 @@ defmodule TellerApiSandbox.Accounts do
     balance = 100_000..999_999 |> Enum.random() |> Kernel./(100) |> to_string()
 
     %{
-      available: balance,
-      ledger: balance
+      "available" => balance,
+      "ledger" => balance
     }
   end
 
@@ -56,8 +56,8 @@ defmodule TellerApiSandbox.Accounts do
 
   defp routing_numbers do
     %{
-      ach: 100_000_000..999_999_999 |> Enum.random() |> to_string(),
-      wire: 100_000_000..999_999_999 |> Enum.random() |> to_string()
+      "ach" => 100_000_000..999_999_999 |> Enum.random() |> to_string(),
+      "wire" => 100_000_000..999_999_999 |> Enum.random() |> to_string()
     }
   end
 
@@ -69,8 +69,8 @@ defmodule TellerApiSandbox.Accounts do
   defp institution do
     institution = Enum.random(@institutions)
     %{
-      id: institution |> String.downcase() |> String.replace(~r/\s+/, "_"),
-      name: institution
+      "id" => institution |> String.downcase() |> String.replace(~r/\s+/, "_"),
+      "name" => institution
     }
   end
 end
